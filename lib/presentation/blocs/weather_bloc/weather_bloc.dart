aef76fab5f904d69e875ad4a3858825c6aa7417d
Dmitry <49454491+unisheld@users.noreply.github.com>
2025-06-10 16:39:53 +0300

    renamed bloc to blocs

--- lib/presentation/blocs/weather_bloc/weather_bloc.dart
+++ lib/presentation/blocs/weather_bloc/weather_bloc.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:wapp/presentation/bloc/weather_bloc/weather_event.dart';
-import 'package:wapp/presentation/bloc/weather_bloc/weather_state.dart';
+import 'package:wapp/presentation/blocs/weather_bloc/weather_event.dart';
+import 'package:wapp/presentation/blocs/weather_bloc/weather_state.dart';
 import '../../../domain/usecases/get_weather_usecase.dart';
 
 class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
